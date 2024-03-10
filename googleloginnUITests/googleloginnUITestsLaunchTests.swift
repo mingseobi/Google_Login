f7145797d534b9d444cbef3a56abaae83c5e7780
mingseobi <22200078@handong.ac.kr>
2024-03-10 19:13:51 +0900

    [init] Google_Login1

---
+++ googleloginnUITests/googleloginnUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  googleloginnUITestsLaunchTests.swift
+//  googleloginnUITests
+//
+//  Created by 김민섭 on 3/10/24.
+//
+
+import XCTest
+
+final class googleloginnUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
